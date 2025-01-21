359985252774a8f3e8d948eb80ca40b7aca829f9
Karl Hepler <karl.hepler@itbit.com>
2025-01-21 00:53:23 -0500

    it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,47 @@
 use chrono::prelude::*;
-use octocrab::{commits::CommitHandler, models::repos::RepoCommit, Error as OctocrabError};
+// use octocrab::{commits::CommitHandler, models::repos::RepoCommit, Error as OctocrabError};
 
 #[derive(Debug)]
-pub struct Request<'a> {
-    pub token: &'a str,
+pub struct Request {
+    pub token: String,
     pub since: NaiveDate,
     pub until: NaiveDate,
 }
 
+#[derive(Debug)]
 pub struct Error {
-    //
+    pub err: Box<dyn std::error::Error>,
 }
 
-pub async fn run(req: Request<'_>) -> Option<Error> {
+pub async fn run(req: Request) -> Option<Error> {
     println!("Hello, world! {:#?}", req);
-    let octocrab = octocrab::instance().user_access_token(req.token)?;
+
+    let octocrab = octocrab::instance().user_access_token(req.token);
+    let octocrab = match octocrab {
+        Ok(octocrab) => octocrab,
+        Err(err) => return Some(Error { err: Box::new(err) }),
+    };
+
     let repos = octocrab
         .current()
         .list_repos_for_authenticated_user()
         .send()
         .await;
+    let repos = match repos {
+        Ok(repos) => repos,
+        Err(err) => return Some(Error { err: Box::new(err) }),
+    };
 
-    match repos {
-        Ok(page) => {
-            println!("page: {:#?}", page)
-        }
-        Err(msg) => eprintln!("error: {:#?}", msg),
-    }
+    eprintln!("{:#?}", repos);
+
+    // repos.items.into_iter().
 
     // let _ = octocrab
     //     .commits("karlhepler", "disfunction")
     //     .list(req.since, req.until)
     //     .collect();
+
+    None
 }
 
 // trait ListCommits<'octo> {
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use chrono::prelude::*;
 use disfunction::{run, Request};
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    if let Some(req) = Request::build() {
-        run(req).await;
-        return;
+    let token = env::var("GITHUB_TOKEN").unwrap();
+    let req = Request::build(&token).unwrap();
+    if let Some(err) = run(req).await {
+        eprintln!("{:#?}", err)
     }
-
-    eprintln!("[ERROR] invalid request")
 }
 
 trait RequestBuilder {
-    fn build() -> Option<Request>;
+    fn build(token: &str) -> Option<Request>;
 }
 
 impl RequestBuilder for Request {
-    fn build() -> Option<Request> {
+    fn build(token: &str) -> Option<Request> {
         Some(Request {
+            token: token.to_string(),
             since: NaiveDate::from_ymd_opt(2025, 01, 01)?,
             until: Utc::now().date_naive(),
         })
